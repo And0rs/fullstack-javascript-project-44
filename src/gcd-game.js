172c4cd96e31d0cc54ad3ea3a297cce424692944
Andrey_Khobotov <and0rscg@gmail.com>
2024-03-21 18:46:07 +0300

    update gcd-game.js

--- src/gcd-game.js
+++ src/gcd-game.js
@@ -1,29 +1,33 @@
 import sayGreetings from './cli.js';
 import { questionAnswer, getRandom } from './index.js';
 
+let lowerNumber = 0;
+let higherNumber = 0;
+
+const sortPair = (firstNumber, secondNumber) => {
+  if (firstNumber > secondNumber) {
+    higherNumber = firstNumber;
+    lowerNumber = secondNumber;
+  } else {
+    higherNumber = secondNumber;
+    lowerNumber = firstNumber;
+  }
+};
+
 export default () => {
   const userName = sayGreetings();
   console.log('Find the greatest common divisor of given numbers.');
-  let lowerNumber = 0;
-  let higherNumber = 0;
   let counter = 0;
   let rightAnswer = 0;
-  
+
   do {
     const firstNumber = getRandom(100);
     const secondNumber = getRandom(100);
+    sortPair(firstNumber, secondNumber);
     const question = `${firstNumber} ${secondNumber}`;
 
-    if (firstNumber > secondNumber) {
-      higherNumber = firstNumber;
-      lowerNumber = secondNumber;
-    } else {
-      higherNumber = secondNumber;
-      lowerNumber = firstNumber;
-    };
-
     while (lowerNumber !== 0) {
-      let tempNum = lowerNumber;
+      const tempNum = lowerNumber;
       lowerNumber = higherNumber % lowerNumber;
       higherNumber = tempNum;
       rightAnswer = higherNumber;
@@ -31,5 +35,4 @@ export default () => {
 
     counter = questionAnswer(question, String(rightAnswer), userName, counter);
   } while (counter < 3 && counter !== 0);
-
-}
+};
