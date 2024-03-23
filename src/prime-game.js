b0bdf0bc10f92d7d94706ef556275ff3aac3576b
Andrey_Khobotov <and0rscg@gmail.com>
2024-03-23 17:34:23 +0300

    add new brain-prime game

---
+++ src/prime-game.js
@@ -0,0 +1,29 @@
+import sayGreetings from './cli.js';
+import { questionAnswer, getRandom } from './index.js';
+
+const isPrime = (num) => {
+  for (let i = 2; i <= num / 2; i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+export default () => {
+  const userName = sayGreetings();
+  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+  let counter = 0;
+  let rightAnswer = '';
+
+  do {
+    const number = getRandom(100 + 2);
+    if (isPrime(number)) {
+      rightAnswer = 'yes';
+    } else {
+      rightAnswer = 'no';
+    }
+
+    counter = questionAnswer(number, rightAnswer, userName, counter);
+  } while (counter < 3 && counter !== 0);
+};
