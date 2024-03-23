b35154e697058a46f17201047104a821a5b40c03
Andrey_Khobotov <and0rscg@gmail.com>
2024-03-23 15:47:46 +0300

    add brain-progression game

---
+++ src/progression-game.js
@@ -0,0 +1,31 @@
+import sayGreetings from './cli.js';
+import { questionAnswer, getRandom } from './index.js';
+
+let start = 0;
+let end = 0;
+let step = 0;
+let hiddenNumber = 0;
+
+export default () => {
+  const userName = sayGreetings();
+  console.log('What number is missing in the progression?');
+  let counter = 0;
+
+  do {
+    const progression = [];
+    start = getRandom(90);
+    step = getRandom(10);
+    end = start + (step * 10);
+    hiddenNumber = getRandom(10);
+
+    for (let i = start; i <= end; i += step) {
+      progression.push(i);
+    }
+
+    const rightAnswer = progression[hiddenNumber];
+    progression[hiddenNumber] = '..';
+    const question = progression.join(' ');
+
+    counter = questionAnswer(question, String(rightAnswer), userName, counter);
+  } while (counter < 3 && counter !== 0);
+};
